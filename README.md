c08532b997bf94f6d036174664a3fe09ff10bf0f
sayebms1@ufl.edu <sayebms1@ufl.edu>
2022-08-04 11:51:10 -0400

    preprocessing for twitter

---
+++ README.md
@@ -0,0 +1,4 @@
+# Preprocessing Text Python Package
+#### Link: check for preprocessing_package in my github profile
+
+This python package is prepared by Aaron Sayeb. 
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+    long_description = file.read() # the redme description will directly come here
+
+
+
+setuptools.setup(
+    name = 'preprocess_syb', # this should be unique and its the name of the pytho pakcage
+    version = '0.0.1',
+    author = 'Mohammad Sayeb',
+    author_email = 'sayebms@gmail.com' ,
+    description = 'This is a pre-processing package',
+    long_description=long_description,
+    long_description_content_type = 'text/markdown', # its a markdown file
+    package = setuptools.find_packages(), # find the directory of the package autmatically
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'],
+    python_requires = '>=3.5'
+)
